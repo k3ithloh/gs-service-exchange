1b0a25070fac656cc755953c69289ba8974e922a
Jonathan <jonathantan1425@gmail.com>
2022-08-27 00:36:45 +0800

    Refactor code to combine pages

--- frontend/src/components/index/navbar.js
+++ frontend/src/components/index/navbar.js
@@ -1,16 +1,9 @@
 import SearchBar from './searchbar';
-
-function NavLink({to, children,}){
-  return (
-    <a href={to} className={`mx-1`}>
-      {children}
-    </a>
-  );
-}
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="flex h-20 items-center justify-between bg-white px-4 py-4 drop-shadow-md">
+    <nav className="flex h-20 items-center justify-between px-4 py-4 drop-shadow-md">
       {/* Logo */}
       <div className="flex items-center justify-start">
         <a className="text-2xl font-semibold" href="/">
@@ -22,18 +15,18 @@ export default function Navbar() {
         <SearchBar />
       </div>
 
-      <div className="flex justify-end">
+      <div className="flex justify-end gap-x-5 ">
 
-        <NavLink to="/contact">
+        <Link href="/signup">
           <button className="w-20 rounded border border-blue-500 bg-transparent py-2 font-semibold text-blue-700 hover:bg-gray-100">
             Sign Up
           </button>
-        </NavLink>
-        <NavLink to="/about">
+        </Link>
+        <Link href="/login">
           <button className="w-20 rounded bg-blue-500 py-2 font-bold text-white hover:bg-blue-700">
             Login
           </button>
-        </NavLink>
+        </Link>
       </div>
     </nav>
   );
