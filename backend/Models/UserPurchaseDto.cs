25184a6e052a747fccc96fa66ab63f7560ed3f32
cherylperyl <cheryl.goh.2020@smu.edu.sg>
2022-08-28 15:39:26 +0800

    adds apis for when user makes a purchase

---
+++ backend/Models/UserPurchaseDto.cs
@@ -0,0 +1,9 @@
+namespace rainbow_unicorn;
+
+public class UserPurchaseDto
+{
+    public string CustomerName { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public float PurchaseAmount { get; set; }
+    public int NumberOfPayments { get; set; }
+}
