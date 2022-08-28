d3875efa8fc0c4e11804305fa8946b22ee153a4c
randall yeo <randall.yeo@isobar.com>
2022-08-28 12:08:42 +0800

    add new pagination

--- frontend/src/pages/financial-data/documentation/index.js
+++ frontend/src/pages/financial-data/documentation/index.js
@@ -10,17 +10,17 @@ const index = () => {
   return (
     <div>
       <Navbar />
-      <div className="text-lg flex container mx-auto px-4 w-full justify-center my-5 border-b">
-        <div className="mr-5">
-          <div className="" style={{cursor: 'pointer'}}>
-            <Link href="/financial-data/overview">Overview</Link>
-          </div>
+      <h1 className="mt-5 text-2xl container mx-auto px-4 w-full">Financial Data Service Documentation</h1>
+      <div className="flex justify-center">
+        <div className="mx-44 flex flex-col pt-10 w-1/5 items-center content-center self-center space-y-4">
+            <h3 className="font-semibold text-dark_blue text-center text-2xl"><Link href="/financial-data/overview">Overview</Link></h3>
         </div>
-        <div className="ml-5">
-          <div className="border-b-white hover:border-b-grey-500" style={{cursor: 'pointer'}}>
-            <Link href="/financial-data/documentation">Documentation</Link>
-          </div>
+        <div className="mx-44 flex flex-col pt-10 w-1/5 items-center content-center self-center space-y-4">
+            <h3 className="font-semibold text-dark_blue text-center text-2xl"><Link href="/financial-data/documentation">Documentation</Link></h3>
+            <a className="bg-dark_blue w-56 h-1 border-dark_blue border flex-grow"></a>
         </div>
+      </div>
+      <div className="absolute border-grey_200 border w-4/5 mx-44">
       </div>
       <div>
         <div className='flex flex-col justify-center mx-auto'>
