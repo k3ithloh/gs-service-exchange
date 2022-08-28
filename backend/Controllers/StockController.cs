406737a1d6bea73e37749f795187cdd5bba3a4c4
cherylperyl <cheryl.goh.2020@smu.edu.sg>
2022-08-28 11:21:16 +0800

    add new stock api

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -1,7 +1,9 @@
+using System.Net;
 using Korzh.EasyQuery.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using rainbow_unicorn.Data;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -10,27 +12,61 @@ namespace rainbow_unicorn.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "rainbow-unicorn-customer")]
+    // [Authorize(Roles = "rainbow-unicorn-customer")]
     public class StockController : ControllerBase
     {
         private readonly DataContext _context;
-        static string _address = "https://www.alphavantage.co/query?function=FX_${timeRange}&from_symbol=${stock}&to_symbol=USD&outputsize=compact&apikey=${API_KEY}";
+        private readonly IConfiguration _configuration;
+        static string _address = String.Format("https://jsonplaceholder.typicode.com/posts/1/comments");
         private string result;
-        public StockController(DataContext context)
+        public StockController(DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStocks()
+        {
+            WebRequest requestObjGet = WebRequest.Create(_address);
+            requestObjGet.Method = "GET";
+            HttpWebResponse responseObjGet = null;
+            responseObjGet = (HttpWebResponse) requestObjGet.GetResponse();
+
+            string strResultTest = null;
+            using (Stream stream = responseObjGet.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                strResultTest = sr.ReadToEnd();
+                sr.Close();
+            }
+
+            return Ok(strResultTest);
+            
+            
+            // byte[] API_KEY = System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:API_KEY").Value);
+            // List<Stock> stockList = new List<Stock>();
+            // using (var httpClient = new HttpClient())
+            // {
+            //     using (var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/1/comments"))
+            //     {
+            //         string apiResponse = await response.Content.ReadAsStringAsync();
+            //         stockList = JsonConvert.DeserializeObject<List<Stock>>(apiResponse);
+            //     }
+            // }
+            // return Ok(stockList);
         }
 
         // Get all Stocks
-        [HttpGet]
-        [SwaggerOperation("Gets all stocks from external API.")]
-        public async Task<ActionResult<List<Stock>>> Get()
-        {
-            // GetResponse();
-            // return new string[] { result, "value2" };
-            return Ok(await _context.Stocks
-                .ToListAsync());
-        }
+        // [HttpGet]
+        // [SwaggerOperation("Gets all stocks from external API.")]
+        // public async Task<ActionResult<List<Stock>>> Get()
+        // {
+        //     // GetResponse();
+        //     // return new string[] { result, "value2" };
+        //     return Ok(await _context.Stocks
+        //         .ToListAsync());
+        // }
 
 
         // Get a Stock based on a given Stock Ticker (ticker)
